1bc85ce634a257603f97e501e65a6e4272cc4a50
shuitiangong <757153944@qq.com>
2025-03-03 01:02:06 +0800

    新建unlit shader

---
+++ Assets/Custom RP/Shader/Unlit.shader
@@ -0,0 +1,6 @@
+Shader "Custom RP/Unlit" {
+    Properties {}
+    SubShader {
+        Pass {}
+    }
+}
---
+++ Assets/Custom RP/Shader/UnlitPass.hlsl
@@ -0,0 +1,11 @@
+﻿#ifndef CUSTOM_UNLIT_PASS_INCLUDE
+#define CUSTOM_UNLIT_PASS_INCLUDE
+float4 UnlitPassVertex(float3 positionOS : POSITION) : SV_POSITION {
+    float3 positionWS = TransformObjectToWorld(positionOS.xyz);
+    return TransformWorldToHClip(positionWS);
+}
+
+void UnlitPassFragment() {
+    
+}
+#endif
