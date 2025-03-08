fd8b21a96f19994b95d34a5fcaa1d5195860bef2
shuitiangong <757153944@qq.com>
2025-03-08 13:39:17 +0800

    MaterialPropertyBlock

---
+++ Assets/Custom RP/Example/PewObjectMaterialProperties.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PewObjectMaterialProperties : MonoBehaviour {
+    private  static int _baseColorID = Shader.PropertyToID("_BaseColor");
+    
+    [SerializeField]
+    private Color baseColor = Color.white;
+    private static MaterialPropertyBlock _block;
+
+    private void Awake() {
+        OnValidate();
+    }
+    
+    private void OnValidate() {
+        if (_block == null) {
+            _block = new MaterialPropertyBlock();
+        }
+        _block.SetColor(_baseColorID, baseColor);
+        GetComponent<Renderer>().SetPropertyBlock(_block);
+    }
+}
